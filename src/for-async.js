4b4838cafa813cc339836eda910ee4d6a71c2d1f
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-06-18 22:58:12 +0200

    0.1.0: Working version with tests

---
+++ src/for-async.js
@@ -0,0 +1,13 @@
+﻿export default function forAsync(arr, work) {
+	function loop(arr, i) {
+		return new Promise((resolve, reject) => {
+			if (i >= arr.length) {resolve()}
+			else try {
+				Promise.resolve(work(arr[i], i))
+				.then(() => resolve(loop(arr, i+1)))
+				.catch(reject);
+			} catch(error) {reject(error)}
+		})
+	}
+	return loop(arr, 0);
+}
---
+++ src/for-async.spec.js
@@ -0,0 +1,87 @@
+﻿import { expect } from 'chai';
+import log from 'picolog';
+import forAsync from './for-async';
+
+describe('forAsync', done => {
+	it('can be used to iterate over an array', () => {
+		const arr = ['one', 'two', 'three'];
+		const expectedResult = arr.join('');
+		let result = '';
+		forAsync(arr, (item, idx) => {
+			result += item;
+		})
+		.then(function() {
+			expect(result).to.equal(expectedResult);
+			done();
+		});
+	});
+
+	it('can execute async work on each item', () => {
+		const arr = ['one', 'two', 'three'];
+		const expectedResult = arr.join('');
+		let result = '';
+		forAsync(arr, (item, idx) => {
+			return new Promise(resolve => setTimeout(() => {
+				result += item;
+				resolve();
+			}, 50))
+		})
+		.then(function() {
+			expect(result).to.equal(expectedResult);
+			done();
+		});
+	});
+
+	it('passes `item` and `idx` to the callback on each iteration', () => {
+		const arr = ['one', 'two', 'three'];
+		const expectedResult = arr.join('');
+		const expectedIndexes = "012";
+		let result = '';
+		let resultIndexes = '';
+		forAsync(arr, (item, idx) => {
+			return new Promise(resolve => setTimeout(() => {
+				result += item;
+				resultIndexes += idx;
+				resolve();
+			}, 50))
+		})
+		.then(function() {
+			expect(result).to.equal(expectedResult);
+			expect(resultIndexes).to.equal(expectedIndexes);
+			done();
+		});
+	});
+
+	it('can be aborted by throwing from sync code', () => {
+		const arr = ['one', 'two', 'three'];
+		forAsync(arr, (item, idx) => {
+			throw new Error('aborted');
+		})
+		.then(function() {
+			expect(false).to.equal(true);
+			done();
+		})
+		.catch(function(error) {
+			expect(error.message).to.equal('aborted');
+			done();
+		});
+	});
+
+	it('can be aborted by rejecting the promise from async code', () => {
+		const arr = ['one', 'two', 'three'];
+		forAsync(arr, (item, idx) => {
+			return new Promise((resolve, reject) => setTimeout(() => {
+				reject('aborted');
+			}, 50))
+		})
+		.then(function() {
+			expect(false).to.equal(true);
+			done();
+		})
+		.catch(function(error) {
+			expect(error.message).to.equal('aborted');
+			done();
+		});
+	});
+});
+
